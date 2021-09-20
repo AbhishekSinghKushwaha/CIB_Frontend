faac7749c6e25e0ce7b887e47513d55c64e81bcf
Jerry Odhiambo <jerryodhiambo5@gmail.com>
2021-09-20 20:44:24 +0300

    Feat:: Adding custom input fields && setting custome equity theme

--- src/app/presentation/modules/pre-login/pre-login.module.ts
+++ src/app/presentation/modules/pre-login/pre-login.module.ts
@@ -5,6 +5,8 @@ import { PreLoginRoutingModule } from './pre-login-routing.module';
 import { PreLoginComponent } from './pre-login.component';
 import { SharedComponentsModule } from '../../shared/components/shared-components.module';
 import { LayoutModule } from '../../layout/layout.module';
+import { FormElementsModule } from '../../shared/form-elements/form-elements.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,10 @@ import { LayoutModule } from '../../layout/layout.module';
     CommonModule,
     PreLoginRoutingModule,
     SharedComponentsModule,
-    LayoutModule
+    LayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FormElementsModule
   ]
 })
 export class PreLoginModule { }
