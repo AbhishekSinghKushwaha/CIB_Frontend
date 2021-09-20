260877a92bcd2337db9a9edf265872e6ab579e9c
Oluwatosin Iyiola <oluwatosin.iyiola@azenia.com>
2021-09-20 14:28:38 +0100

    Security challenge component created

--- src/app/presentation/modules/pre-login/security-challenge/security-challenge-routing.module.ts
+++ src/app/presentation/modules/pre-login/security-challenge/security-challenge-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SecurityChallengeComponent } from './security-challenge.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+    path: '',
+    component: SecurityChallengeComponent
+  }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
