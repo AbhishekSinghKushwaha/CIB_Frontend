217d2d34ec44332e763d0e2067b56ca4177047fc
Jerry Odhiambo <jerryodhiambo5@gmail.com>
2021-10-19 10:35:23 +0300

    Adding production environment variables to fix prod builds

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://api-omnichannel-dev.azure-api.net',
+  clientId: 'FAC1D96F0E0A44E',
+  appInstance: 'one-equity',
+  appName: 'Equity online',
+  appInsights: {
+    instrumentationKey: 'd77c559c-dceb-4ad1-8f51-c2774055f6c7',
+  },
 };
+
+export const secugenUri = {
+  production: false,
+  apiUri:
+    'timeout=10000&quality=50&licstr=&templateFormat=ISO&imageWSQRate=0.95',
+};
+
