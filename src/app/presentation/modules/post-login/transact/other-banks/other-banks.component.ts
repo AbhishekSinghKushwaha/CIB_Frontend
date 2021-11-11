e719914f7dafa9a86ab17fd4531ce7a6d68e6cbb
Jerry Odhiambo <jerryodhiambo5@gmail.com>
2021-11-11 15:31:32 +0300

    EFT UI design translation

--- src/app/presentation/modules/post-login/transact/other-banks/other-banks.component.ts
+++ src/app/presentation/modules/post-login/transact/other-banks/other-banks.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-other-banks',
   templateUrl: './other-banks.component.html',
-  styleUrls: ['./other-banks.component.scss']
+  styleUrls: ['./other-banks.component.scss'],
 })
 export class OtherBanksComponent implements OnInit {
-
-  constructor() { }
+  loading: boolean = false;
+  interBankTransferForm: FormGroup;
+  constructor(private readonly fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  initForm() {
+    this.interBankTransferForm = this.fb.group({
+      transactionType: ['', [Validators.required]],
+      sendFrom: ['', [Validators.required]],
+      sendTo: ['', Validators.required],
+      amount: [{}, [Validators.required]],
+      fxReferenceId: ['', [Validators.required]],
+      reason: [''],
+    });
+  }
+
+  // Get transfer charge, then proceed to review payment
+  getTransferCharges() {}
 }
--- src/app/presentation/modules/post-login/transact/other-banks/other-banks.module.ts
+++ src/app/presentation/modules/post-login/transact/other-banks/other-banks.module.ts
@@ -3,15 +3,30 @@ import { CommonModule } from '@angular/common';
 
 import { OtherBanksRoutingModule } from './other-banks-routing.module';
 import { OtherBanksComponent } from './other-banks.component';
-
+import { MatStyleModule } from 'src/app/mat-style.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormElementsModule } from 'src/app/presentation/shared/form-elements/form-elements.module';
+import { SharedComponentsModule } from 'src/app/presentation/shared/components/shared-components.module';
+import { SelectAccountModalModule } from 'src/app/presentation/shared/modals/select-account-modal/select-account-modal.module';
+import { CurrencySelectionConstants } from 'src/app/core/utils/constants/currency-selection.constants';
+import { SelectAccountConstants } from 'src/app/data/repository/select-account-mock-repository/select-account.constants';
+import { FavouritesModalModule } from 'src/app/presentation/shared/modals/favourites-modal/favourites-modal.module';
+import { CurrencySelectionModule } from 'src/app/presentation/shared/modals/currency-selection/currency-selection.module';
 
 @NgModule({
-  declarations: [
-    OtherBanksComponent
-  ],
+  declarations: [OtherBanksComponent],
   imports: [
     CommonModule,
-    OtherBanksRoutingModule
-  ]
+    OtherBanksRoutingModule,
+    MatStyleModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FormElementsModule,
+    SharedComponentsModule,
+    SelectAccountModalModule,
+    FavouritesModalModule,
+    CurrencySelectionModule,
+  ],
+  providers: [CurrencySelectionConstants, SelectAccountConstants],
 })
-export class OtherBanksModule { }
+export class OtherBanksModule {}
