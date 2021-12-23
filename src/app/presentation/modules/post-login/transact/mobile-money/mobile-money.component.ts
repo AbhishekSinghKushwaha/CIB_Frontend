f4da3f04099dfc909618cd20608cf0ec79a2461a
Jerry Odhiambo <jerry.odhiambo@azenia.com>
2021-12-23 17:18:18 +0300

    Mobile money transfer ui translation

--- src/app/presentation/modules/post-login/transact/mobile-money/mobile-money.component.ts
+++ src/app/presentation/modules/post-login/transact/mobile-money/mobile-money.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { accountLimitValidator } from 'src/app/core/utils/validators/limits.validators';
 
 @Component({
   selector: 'app-mobile-money',
   templateUrl: './mobile-money.component.html',
-  styleUrls: ['./mobile-money.component.scss']
+  styleUrls: ['./mobile-money.component.scss'],
 })
 export class MobileMoneyComponent implements OnInit {
-
-  constructor() { }
+  mobileMoneyTransferForm: FormGroup;
+  constructor(private readonly fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  initForm() {
+    this.mobileMoneyTransferForm = this.fb.group({
+      transactionType: ['', [Validators.required]],
+      sendFrom: ['', [Validators.required]],
+      sendTo: ['', Validators.required],
+      amount: [{}, [Validators.required, accountLimitValidator]],
+      fxReferenceId: ['', [Validators.required]],
+      schedulePayment: ['', [Validators.required]],
+      reason: [''],
+    });
+  }
+
+  getTransferCharges() {}
 }
--- src/app/presentation/modules/post-login/transact/mobile-money/mobile-money.module.ts
+++ src/app/presentation/modules/post-login/transact/mobile-money/mobile-money.module.ts
@@ -3,15 +3,28 @@ import { CommonModule } from '@angular/common';
 
 import { MobileMoneyRoutingModule } from './mobile-money-routing.module';
 import { MobileMoneyComponent } from './mobile-money.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatStyleModule } from 'src/app/mat-style.module';
+import { SharedComponentsModule } from 'src/app/presentation/shared/components/shared-components.module';
+import { FormElementsModule } from 'src/app/presentation/shared/form-elements/form-elements.module';
+import { CurrencySelectionModule } from 'src/app/presentation/shared/modals/currency-selection/currency-selection.module';
+import { FavouritesModalModule } from 'src/app/presentation/shared/modals/favourites-modal/favourites-modal.module';
+import { SchedulePaymentModule } from 'src/app/presentation/shared/modals/schedule-payment/schedule-payment.module';
+import { SelectAccountModalModule } from 'src/app/presentation/shared/modals/select-account-modal/select-account-modal.module';
 
 @NgModule({
-  declarations: [
-    MobileMoneyComponent
-  ],
+  declarations: [MobileMoneyComponent],
   imports: [
     CommonModule,
-    MobileMoneyRoutingModule
-  ]
+    MobileMoneyRoutingModule,
+    SharedComponentsModule,
+    MatStyleModule,
+    ReactiveFormsModule,
+    FavouritesModalModule,
+    SelectAccountModalModule,
+    CurrencySelectionModule,
+    SchedulePaymentModule,
+    FormElementsModule,
+  ],
 })
-export class MobileMoneyModule { }
+export class MobileMoneyModule {}
