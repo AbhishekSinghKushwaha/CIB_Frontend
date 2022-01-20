3e8cd769ed6b6c6b5a3fc5bafca1dc72899e59c7
Ivan Georgiev <Ivan.Georgiev@azenia.com>
2022-01-20 21:15:22 +0100

    Add customer onboarding company details pages

--- src/app/presentation/modules/customer-onboarding/services/customer-onboarding.service.ts
+++ src/app/presentation/modules/customer-onboarding/services/customer-onboarding.service.ts
@@ -1,27 +1,49 @@
 import { Injectable } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { CompanyDetailsSummaryComponent } from '../components/customer-onboarding-register-company-details/company-details-summary/company-details-summary.component';
 import { CustomerOnboardingRegistrationModalComponent } from '../components/customer-onboarding-registration-modal/customer-onboarding-registration-modal.component';
 
 @Injectable()
 export class CustomerOnboardingService {
   public showOnboarding: boolean;
 
-  modal: MatDialogRef<CustomerOnboardingRegistrationModalComponent, any>;
+  registrationModal: MatDialogRef<
+    CustomerOnboardingRegistrationModalComponent,
+    any
+  >;
+
+  companyDetailsModal: MatDialogRef<CompanyDetailsSummaryComponent, any>;
 
   constructor(private readonly dialog: MatDialog) {}
 
-  open() {
-    this.modal = this.dialog.open<
+  openRegistrationModal() {
+    this.registrationModal = this.dialog.open<
       CustomerOnboardingRegistrationModalComponent,
       any
     >(CustomerOnboardingRegistrationModalComponent, {
       maxWidth: '900px',
       disableClose: true,
     });
-    return this.modal;
+    return this.registrationModal;
   }
 
-  close() {
-    this.modal.close();
+  closeRegistrationModal() {
+    this.registrationModal.close();
+  }
+
+  openCompanyDetailsModal(data: any) {
+    this.companyDetailsModal = this.dialog.open<
+      CompanyDetailsSummaryComponent,
+      any
+    >(CompanyDetailsSummaryComponent, {
+      data,
+      maxWidth: '900px',
+      disableClose: true,
+    });
+    return this.companyDetailsModal;
+  }
+
+  closeCompanyDetailsModal() {
+    this.companyDetailsModal.close();
   }
 }
