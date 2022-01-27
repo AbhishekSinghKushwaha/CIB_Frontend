ad6591d58c0bcfcea9aaa2e1eeede118c4e1002b
Jerry Odhiambo <jerry.odhiambo@azenia.com>
2022-01-27 07:31:37 +0300

    Customer onboarding modals

---
+++ src/app/core/domain/customer-onboarding.model.ts
@@ -0,0 +1,10 @@
+export interface accountOption {}
+
+export interface companyDetails {
+  registrationNumber?: string;
+  companyName?: string;
+  countryId?: string;
+  phoneNumber?: string;
+  emailAddress?: string;
+  address?: string;
+}
