f0c9c15d132a916902558f8b5e643486bc0dd835
Oluwatosin Iyiola <oluwatosin.iyiola@azenia.com>
2022-01-31 12:16:16 +0100

    Chequebook collection done

---
+++ src/app/presentation/shared/modals/branch-modal/branch-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-branch-modal',
+  templateUrl: './branch-modal.component.html',
+  styleUrls: ['./branch-modal.component.scss']
+})
+export class BranchModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
