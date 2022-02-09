2c6837bf97be3272d534bf853a7df3ce8e40e687
moussa doumbia <moussa.doumbia9219@gmail.com>
2022-02-09 14:36:21 +0000

    merge develop and resolve build issues

---
+++ src/app/presentation/shared/components/language-input/language-input.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-language-input',
+  templateUrl: './language-input.component.html',
+  styleUrls: ['./language-input.component.scss']
+})
+export class LanguageInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  openLanguageModal() {
+    
+  }
+
+}
