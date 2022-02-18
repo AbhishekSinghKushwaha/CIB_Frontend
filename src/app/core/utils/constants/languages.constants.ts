d5af8f67be859b39ebbaa044ed4b74e5a6b1ef05
Oluwatosin Iyiola <oluwatosin.iyiola@azenia.com>
2022-02-18 09:41:44 +0100

    More language classes added

--- src/app/core/utils/constants/languages.constants.ts
+++ src/app/core/utils/constants/languages.constants.ts
@@ -5,10 +5,9 @@ export const LANGUAGES: { all: LanguageModel[], default: number } = {
     { id: 1, langCode: 'en', langDesc: 'English' },
     { id: 2, langCode: 'fr', langDesc: 'Français' },
     { id: 3, langCode: 'zh', langDesc: '中文' },
-    { id: 4, langCode: 'fr', langDesc: 'Français' },
-    { id: 5, langCode: 'sw', langDesc: 'Swahili' },
-    { id: 6, langCode: 'rw', langDesc: 'Kinyarwanda' },
-    { id: 7, langCode: 'lg', langDesc: 'Buganda' }
+    { id: 4, langCode: 'sw', langDesc: 'Swahili' },
+    { id: 5, langCode: 'rw', langDesc: 'Kinyarwanda' },
+    { id: 6, langCode: 'lg', langDesc: 'Buganda' }
   ],
   default: 1
 }
