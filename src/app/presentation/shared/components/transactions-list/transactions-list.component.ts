d38f3c91b8bcce061497277a7b9f164dac54e44a
Oluwatosin Iyiola <oluwatosin.iyiola@azenia.com>
2022-03-07 23:40:44 +0100

    transaction search done

--- src/app/presentation/shared/components/transactions-list/transactions-list.component.ts
+++ src/app/presentation/shared/components/transactions-list/transactions-list.component.ts
@@ -1,5 +1,6 @@
 import { PaginationModel } from './../../../../core/domain/pagination.model';
 import { Component, OnInit, Input } from '@angular/core';
+import { TransactionListmodel } from 'src/app/core/domain/transaction-list.model';
 
 @Component({
   selector: 'app-transactions-list',
@@ -7,21 +8,35 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./transactions-list.component.scss']
 })
 export class TransactionsListComponent implements OnInit {
-  searchText: string;
-  _category: string;
-  paginationData: PaginationModel;
-  transactions: {
-    title: string,
-    description: string,
-    amount: string,
-    date: string,
-    status: 'Approved' | 'Rejected' | 'Pending',
-  }[] = [];
-  transactionIcon = { Approved: 'transaction_approved', Rejected: 'transaction_rejected', Pending: '' }
-
+  private _searchText: string;
+  get searchText(): string {
+    return this._searchText;
+  }
+  set searchText(input: string) {
+    const value = input.toLowerCase();
+    this.transactions = [];
+    for (const transaction of this.originalTransactions) {
+      if (transaction.amount.toLowerCase().indexOf(value) > -1 ||
+        transaction.date.toLowerCase().indexOf(value) > -1 ||
+        transaction.description.toLowerCase().indexOf(value) > -1 ||
+        transaction.status.toLowerCase().indexOf(value) > -1 ||
+        transaction.title.toLowerCase().indexOf(value) > -1
+      ) {
+        this.transactions.push(transaction)
+      }
+    }
+    this._searchText = input;
+    this.paginationData = new PaginationModel(10, [10, 25, 40, 60], this.transactions.length);
+  };
+  private _category: string;
   get category() {
     return this._category
   }
+  paginationData: PaginationModel;
+  transactions: TransactionListmodel[] = [];
+  originalTransactions: TransactionListmodel[] = [];
+  transactionIcon = { Approved: 'transaction_approved', Rejected: 'transaction_rejected', Pending: '' };
+
   @Input() set category(category: string) {
     this._category = category;
     this.loadTransactions();
@@ -34,20 +49,21 @@ export class TransactionsListComponent implements OnInit {
 
   loadTransactions() {
     this.category === 'history' ?
-      this.transactions = Array(35).fill(0).map((x, i) => ({
+      this.originalTransactions = Array(35).fill(0).map((x, i) => ({
         title: 'Reference C38437393944034',
         description: 'MOBILE WALLET TRANSFER TO LESLIE ISAH',
         amount: '000,000.00 KES',
         date: 'Thu,23 Aug',
         status: i % 2 === 0 ? 'Approved' : 'Rejected'
       })) :
-      this.transactions = Array(32).fill(0).map((x, i) => ({
+      this.originalTransactions = Array(32).fill(0).map((x, i) => ({
         title: 'Reference C38437393944034',
         description: 'MOBILE WALLET TRANSFER TO LESLIE ISAH',
         amount: '000,000.00 KES',
         date: 'Thu,23 Aug',
         status: 'Pending'
       }));
+    this.transactions = [...this.originalTransactions];
     this.paginationData = new PaginationModel(10, [10, 25, 40, 60], this.transactions.length);
   }
 
