4c0ac5a6842c92c41c1d3fd0fd9ccdfb1c2d8427
Jerry Odhiambo <jerry.odhiambo@azenia.com>
2022-03-18 13:29:13 +0300

    Adding payments and transactions to dashboard

---
+++ src/app/presentation/modules/post-login/home/dashboard/payments-and-transactions/payments-and-transactions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payments-and-transactions',
+  templateUrl: './payments-and-transactions.component.html',
+  styleUrls: ['./payments-and-transactions.component.scss']
+})
+export class PaymentsAndTransactionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
