364ebb49f351a6485db599d0ba75c11a4bd584fa
Jerry Odhiambo <jerry.odhiambo@azenia.com>
2022-05-06 12:18:12 +0300

    Bug fix: Environment file uat update

--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -1,10 +1,11 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api-uat.equitygroupholdings.com/v1',
-  clientId: 'FAC1D96F0E0A44E',
-  appInstance: 'one-equity',
-  appName: 'Corporate Internet Banking',
-  kService: 'b_tDiAKr=yiM',
+  apiUrl: "https://api-uat.equitygroupholdings.com/v1",
+  clientId: "FAC1D96F0E0A44E",
+  appInstance: "one-equity",
+  appName: "Corporate Internet Banking",
+  kService: "b_tDiAKr=yiM",
   logoutWarningTimeMinutes: 4,
-  logoutForcedTimeMinutes: 5
+  logoutForcedTimeMinutes: 5,
+  minutesBeforeForcedLogout: 2,
 };
