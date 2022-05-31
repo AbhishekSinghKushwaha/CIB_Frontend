d6618aaa43f75efcd958a0da2edaddd483de2cdb
Oluwatosin Iyiola <oluwatosin.iyiola@azenia.com>
2022-05-31 10:50:54 +0100

    Mocked userMappedAccounts

--- src/app/core/services/accounts/accounts.service.ts
+++ src/app/core/services/accounts/accounts.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import urlList from '../service-list.json';
 @Injectable({
@@ -11,8 +11,11 @@ export class AccountsService {
   constructor(private http: HttpClient) { }
 
   // Get user accounts
+  // getUserAccounts(): Observable<any> {
+  //   return this.http.get(environment.apiUrl + urlList.accounts.getUserAccounts);
+  // } 
   getUserAccounts(): Observable<any> {
-    return this.http.get(environment.apiUrl + urlList.accounts.getUserAccounts);
+    return of({ "data": [{ "accountName": "JAMES EKAI NATUKOI", "accountNumber": "1100194977404", "accountType": "", "balance": 724979.46, "transactionLimit": 10000.0, "currency": "KES" }], "status": true, "message": "Request was Successful", "responseCode": "00", "response": 1 })
   }
 
 }
