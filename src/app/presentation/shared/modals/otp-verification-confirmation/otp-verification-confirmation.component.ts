128907fb56bfbd4ab406f8b96e5865ce653a91ac
feyisewa <feyisewaawotedu@gmail.com>
2022-06-22 18:54:49 +0200

    updated otp modal to send otp to user

--- src/app/presentation/shared/modals/otp-verification-confirmation/otp-verification-confirmation.component.ts
+++ src/app/presentation/shared/modals/otp-verification-confirmation/otp-verification-confirmation.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
+import { CorporateService } from 'src/app/core/services/corporate/corporate.service';
 import { OtpCodeService } from 'src/app/core/services/otp-code/otp-code.service';
+import { OtpVerificationListService } from 'src/app/core/services/otp-verification-list/otp-verification-list.service';
+import { StorageService } from 'src/app/core/services/storage/storage.service';
 
 @Component({
   selector: 'app-otp-verification-confirmation',
@@ -9,14 +13,25 @@ import { OtpCodeService } from 'src/app/core/services/otp-code/otp-code.service'
   styleUrls: ['./otp-verification-confirmation.component.scss']
 })
 export class OtpVerificationConfirmationComponent implements OnInit {
+  selected: any;
+  otpMethod: any;
+  platformId: number;
 
   constructor(   
      private readonly router: Router,
-     private readonly otpCodeService: OtpCodeService,
-     readonly dialogRef: MatDialogRef<OtpVerificationConfirmationComponent>
-     ) { }
+     private readonly corporateService: CorporateService,
+     readonly dialogRef: MatDialogRef<OtpVerificationConfirmationComponent>,
+     private readonly otpVerificationListService: OtpVerificationListService,
+     private storageService: StorageService,
+    private readonly authService: AuthService,
+
+     ) {
+      this.selected = otpVerificationListService.otpContent;
+      }
 
   ngOnInit(): void {
+    const currentUser = this.storageService.getData('currentUserData');
+    this.platformId = currentUser.platformId;
   }
 
   close(): void {
@@ -25,9 +40,22 @@ export class OtpVerificationConfirmationComponent implements OnInit {
 
 
   submit() {
-  localStorage.setItem('sourceRoute', 'more')
-  this.router.navigate([`/more/otp-access-verify/${'sms'}/sms`]);
-  this.close();
+  this.otpMethod = this.selected.length === 2 ? 'ALL' : this.selected[0]
+  this.sendOtp();
+  
+  }
+
+  sendOtp() {
+    this.corporateService.sendOtp(this.platformId).subscribe((response:any) => {
+      
+        this.otpMethod === 'Email' ? this.authService.setOTPMessage(response.message) : this.storageService.removeData('otp_message');
+        this.router.navigate([`/more/otp-access/verify/${this.otpMethod}`]);
+        this.close();
+      },
+      (error) => {
+        console.log(error);
+      }
+    )
   }
 
 }
